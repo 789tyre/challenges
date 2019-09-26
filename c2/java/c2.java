559137e3761a55356fd5f2ed318876e62dd2aeb0
Gavin <win7100123@gmail.com>
2019-09-26 19:22:16 +0300

    Completed c2 in Python and Java

--- c2/java/c2.java
+++ c2/java/c2.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 class main{
     public static void main(String[] args){
-	System.out.println("1. Water\n2. Coca Cola\n3. Pepsi\n4. Pepno\n5. 7Up\n\n");
+	Scanner s = new Scanner(System.in);
+	System.out.println("\nWhat would you like to drink?");
+	System.out.println("1. Water\n2. Coca Cola\n3. Pepsi\n4. Pepno\n5. 7Up\n");
+	int choice = s.nextInt();
+	// switch(choice){
+	//     case 1:
+	// 	System.out.println("You chose water.");
+	// 	break;
+	//     case 2:
+	// 	System.out.println("You got Coca Cola.");
+	// 	break;
+	//     case 3:
+	// 	System.out.println("Si, I will deliver");
+	// 	break;
+	//     case 4:
+	// 	System.out.println("Pepno, really? Sure");
+	// 	break;
+	//     case 5:
+	// 	System.out.println("7Up coming down");
+	// 	break;
+	//     default:
+	// 	System.out.println("Error. choice was not valid, here is your money back.");
+	// 	break;
+	// }
+    
+	// This is to complete the star challenge
+	if(choice == 1){
+	    System.out.println("You chose water.");
+	} else if (choice == 2) {
+	    System.out.println("You got Coca Cola.");
+	} else if (choice == 3) {
+	    System.out.println("Si, I will deliver");
+	} else if (choice == 4) {
+	    System.out.println("Pepno, really? Sure");
+	} else if (choice == 5) {
+	    System.out.println("7Up coming down");
+	} else {
+	    System.out.println("Error. choice was not valid, here is you money back.");
+	}
     }
 }
---
+++ c2/python/c2.py
@@ -0,0 +1,18 @@
+print("Enter your choice: ")
+print("1. Water\n2. Coca Cola\n3. Pepsi\n4. Pepno\n5. 7Up\n")
+choice = int(input())
+if choice == 1:
+    print("You chose water.")
+elif choice == 2:
+    print("You chose Coca Cola.")
+elif choice == 3:
+    print("Si, I will deliver")
+elif choice == 4:
+    print("Pepno, really? Sure")
+elif choice == 5:
+    print("7Up comming down")
+else:
+    print("Error. choice was not valid, here is you money back.")
+
+#Python does not have a switch statment
+
